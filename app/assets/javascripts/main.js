b3125bd5b5447b002c189bb0f5c4bd3019dcc9cc
Philip Hughes <philip@h4w5.com>
2015-02-25 16:27:32 -0500

    now we append pokemon on page load!

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -21,8 +21,6 @@ $(document).ready(function() {
   $pokeballListEl = $('.pokeballs');
 
   $('#generate-random-poke').on('click', function(e) {
-    // get a random pokemon from the database and
-    // attach it to the DOM (with the large template)
     $.ajax({
       url:  '/pokemons?random=true',
       type: 'GET',
@@ -32,6 +30,47 @@ $(document).ready(function() {
       // console.log(data);
       attachLargePokemonTemplate(data);
     });
+  });
+
+  // on the page load
+  // get the list of pokeballs in the database
+  $.ajax({
+    url: '/pokeballs',
+    type: 'GET',
+    dataType: 'JSON'
+  }).done(function(list_of_pokeballs) {
+    console.log(list_of_pokeballs);
+
+    // iterate of the list of pokeballs
+    for(var i=0, len=list_of_pokeballs.length;i<len;i++) {
+
+      // get the pokeball list id
+      var id = list_of_pokeballs[i].id;
+
+      // with that id, make another AJAX call to the pokeball list of pokemons
+      $.ajax({
+        url: '/pokeballs/' + id,
+        type: 'GET',
+        dataType: 'JSON'
+      }).done(function(pokeball_detail) {
+        console.log(pokeball_detail);
+
+        // given all of a pokeball's detailed information
+        // get the pokeballs pokemons
+        var pokemons = pokeball_detail.pokemons;
+        // get the name of the pokeball's owner (as lowercase)
+        var name = pokeball_detail.pokeball.name.toLowerCase();
+
+        // given the owner's name, we grab the HTML element that shows their pokemon
+        var $list = $('[data-name=' + name + ']');
+
+        // iterate over the list of pokemon
+        for (var j=0,len=pokemons.length;j<len;j++){
+          // append each pokemon to the DOM
+          attachSmallPokemonTemplate(pokemons[j], $list.find('.panel-body'));
+        }
+      });
+    }
   });
 });
 
