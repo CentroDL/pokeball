ae56b842af6b60be55315d4edd2d59bb1a7ab528
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-25 15:15:52 -0500

    pre merge

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -21,8 +21,8 @@ var clearRandomPokemon = function() {
 }
 var $templatePokemonLarge = function(attributes, person) {
   $template = $('<div class="pokemon template large animated zoomIn">')
-                .attr('data-rid',     attributes.id) // rails id
-                .attr('data-pkdx-id', attributes.pkdx_id);
+               .attr('data-rid',     attributes.id) // rails id
+               .attr('data-pkdx-id', attributes.pkdx_id);
   $('<h4>').text(attributes.name).appendTo($template);
   $image = $('<img class="poke-sprite" width="120"/>')
              .attr('src', attributes.image_url);
@@ -45,22 +45,31 @@ var $templatePokeball = function(attributes) {
   // create a small jQuery template for pokéballs
 }
 var attachSmallPokemonTemplate = function(attributes, elementAppendedTo) {
-  // call your small template and then append it where it should go
+  clearRandomPokemon();
+  $small = $templatePokemonSmall(attributes);
+  $small.appendTo(elementAppendedTo);
 }
+
 var $templatePokemonSmall = function(attributes) {
-  // create a small jQuery template for Pokémon
-  // to insert into your Poké ball!
+  $template = $('<div>').attr('class', "pokemon template small animated zoomIn")
+                        .attr( "data-rid", attributes.id)
+                        .attr( "data-pkdx-id", attributes.image_url);
+  $("<h4>").text(attributes.name).appendTo($template);
+  $("<img>").attr("src", attributes.image_url).appendTo($template);
+
+    return $template;
 }
 
 // Click methods
 var catchPokemon = function(e) {
   $chosen_player = $('.chosen').children().first();
   $pokemon       = $(e.target).parent();
-
-  // this is a stub...
-  // fires when you click the catch button (see large template above)
-
   console.log($chosen_player.data('name') + ' --> ' + $pokemon.data('rid'));
+
+  $.ajax().done(function(data){
+    attachSmallPokemonTemplate(  currentPokemonData(), $chosen_player.find("panel-body") );
+  });
+
 }
 
 // these methods change the chosen pokeball and return whose pokeball it is
@@ -86,6 +95,15 @@ var iChoseNext = function() {
 }
 var currentPlayer = function($chosen) {
   return $chosen.children().first().data('name');
+}
+
+var currentPokemonData = function(){
+  return {
+    id:         $pokemon.data('rid'),
+    pkdx_id:    $pokemon.data('pkdx-id'),
+    name:       $pokemon.find('h4').text(),
+    image_url: $pokemon.find("img").attr("src")
+  }
 }
 
 $(document).ready(function() {
